770b0de35ca59249555f0edc7cb67c108688ad29
matebonfanti <matebonfanti@gmail.com>
2023-09-27 16:51:37 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -50,7 +50,21 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position=position;
 	}
-	
+
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+					
+		}
+		if(piece(position) == null) {
+			return null;
+			
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >=0 && column < columns;
 	}
@@ -66,4 +80,6 @@ public class Board {
 		
 		
 	}
+	
+
 }
